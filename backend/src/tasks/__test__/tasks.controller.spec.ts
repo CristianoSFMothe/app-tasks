0e49c7d47de6781e4227426b6190032fef992eda
CristianoSFMothe <cristianodevsystem@gmail.com>
2024-01-06 19:51:51 -0300

    :rocket: feat: test unit controller

--- backend/src/tasks/__test__/tasks.controller.spec.ts
+++ backend/src/tasks/__test__/tasks.controller.spec.ts
@@ -1,18 +1,137 @@
-import { Test, TestingModule } from '@nestjs/testing';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NotFoundException } from '@nestjs/common';
+import { StatusTasks } from '../../enun/status.enum';
+import { CreateTaskDto } from '../dtos/create-tasks.dto';
+import { UpdateTaskDetailsDto } from '../dtos/update-tasks.dto';
 import { TasksController } from '../tasks.controller';
+import { TasksService } from '../tasks.service';
+import { randomUUID } from 'crypto';
 
-describe('TasksController', () => {
-  let controller: TasksController;
+describe('TasksController Unit Test', () => {
+  let tasksController: TasksController;
+  let tasksService: TasksService;
+  let id: string;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TasksController],
-    }).compile();
-
-    controller = module.get<TasksController>(TasksController);
+  beforeEach(() => {
+    id = randomUUID();
+    tasksService = new TasksService();
+    tasksController = new TasksController(tasksService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return all tasks', async () => {
+    const mockTasks = [
+      {
+        id: '1',
+        task: 'Sample Task 1',
+        description: 'Description 1',
+      },
+      {
+        id: '2',
+        task: 'Sample Task 2',
+        description: 'Description 2',
+      },
+    ];
+
+    jest.spyOn(tasksService, 'findAllTasks').mockResolvedValue(mockTasks);
+
+    expect(await tasksController.findAll()).toBe(mockTasks);
+  });
+
+  it('should return a task by ID', async () => {
+    const taskId = 'some-id';
+    const result = {
+      id: taskId,
+      task: 'Sample Task',
+      description: 'Sample Description',
+    };
+
+    jest.spyOn(tasksService, 'findOneTask').mockResolvedValue(result);
+
+    expect(await tasksController.findOne(taskId)).toBe(result);
+  });
+
+  it('should create a new task', async () => {
+    const mockTaskDto: CreateTaskDto = {
+      task: 'Sample Task',
+      description: 'Sample Description',
+    };
+
+    const mockTaskResponse = {
+      id: '1',
+      task: 'Sample Task',
+      description: 'Sample Description',
+      status: StatusTasks.PENDING,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    jest.spyOn(tasksService, 'createTask').mockResolvedValue(mockTaskResponse);
+
+    const result = await tasksController.create(mockTaskDto);
+    expect(result).toBe(mockTaskResponse);
+  });
+
+  it('should update a task and return the updated task', async () => {
+    const mockId = '1';
+    const mockUpdateDto: UpdateTaskDetailsDto = {
+      task: 'Updated Task',
+      description: 'Updated Description',
+    };
+
+    const expectedUpdatedTask = {
+      id: mockId,
+      task: 'Updated Task',
+      description: 'Updated Description',
+      status: StatusTasks.PENDING,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    jest
+      .spyOn(tasksService, 'updateTask')
+      .mockResolvedValue(expectedUpdatedTask);
+
+    const result = await tasksController.update(mockId, mockUpdateDto);
+
+    expect(tasksService.updateTask).toHaveBeenCalledWith(mockId, mockUpdateDto);
+
+    expect(result).toEqual(expectedUpdatedTask);
+  });
+
+  it('should remove a task and return the task ID and name', async () => {
+    const mockId = '1';
+    const mockTask = {
+      id: mockId,
+      task: 'Sample Task',
+      description: 'Sample Description',
+      status: 'PENDING',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const expectedResponse = {
+      id: mockId,
+      task: 'Sample Task',
+    };
+
+    jest.spyOn(tasksService, 'removeTask').mockResolvedValue(expectedResponse);
+
+    const result = await tasksController.remove(mockId);
+
+    expect(tasksService.removeTask).toHaveBeenCalledWith(mockId);
+
+    expect(result).toEqual(expectedResponse);
+  });
+
+  it('should throw NotFoundException if task does not exist', async () => {
+    const mockId = '2';
+
+    jest
+      .spyOn(tasksService, 'removeTask')
+      .mockRejectedValue(new NotFoundException(`Task ${mockId} not found`));
+
+    await expect(tasksController.remove(mockId)).rejects.toThrow(
+      NotFoundException,
+    );
   });
 });
